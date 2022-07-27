8068b2c41d2e38f87e5f51c376bc1f113bcb1da1
April Hughes <dreydain@gmail.com>
2022-07-27 13:48:20 -0700

    Completed portfolio sort functionality

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const sections = document.querySelectorAll('section');
 const navBtns = document.querySelectorAll('.link');
 const mobileNav = document.querySelector('.nav-list');
 const hamburger = document.getElementById('hamburger');
-
+const all = document.getElementById('all');
+const design = document.getElementById('design');
+const develop = document.getElementById('develop');
+const games = document.getElementById('games');
+const full_stack = document.getElementById('full-stack');
 const projects = document.querySelectorAll('.project');
-
 mobileNav.style.maxHeight = '0px';
 
 // Navigation
@@ -40,4 +43,45 @@ hamburger.addEventListener('click', () => {
 		mobileNav.style.opacity = '0';
 		mobileNav.style.padding = '0';
 	}
+});
+
+// ----- Portfolio Project Sort Functions ----- //
+
+// Array of all Projects
+const projectsArr = [...projects.entries()];
+let projectList = [];
+projectsArr.forEach((project) => {
+	projectList.push([...project]);
+});
+
+// Function to Sort and display projects dynamically
+function sort(projectType) {
+	projectList.forEach((project) => {
+		project[1].classList.contains(projectType)
+			? (project[1].style.display = 'block')
+			: (project[1].style.display = 'none');
+	});
+}
+
+// OnClick Events calling Sort function
+all.addEventListener('click', () => {
+	projectList.forEach((project) => {
+		project[1].style.display = 'block';
+	});
+});
+
+design.addEventListener('click', () => {
+	sort('design');
+});
+
+develop.addEventListener('click', () => {
+	sort('develop');
+});
+
+games.addEventListener('click', () => {
+	sort('games');
+});
+
+full_stack.addEventListener('click', () => {
+	sort('full-stack');
 });
